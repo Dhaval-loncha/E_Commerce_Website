ac4f6bfb71a1707202cd7924f1737290ac488ecf
Loncha Dhaval <dhavalkloncha@gmail.com>
2024-09-23 11:49:52 +0530

    admin folder added

---
+++ admin/src/components/AddProduct/AddProduct.jsx
@@ -0,0 +1,136 @@
+import "./AddProduct.css";
+import uploadArea from "../../assets/upload_area.svg";
+import { useState } from "react";
+
+const AddProduct = () => {
+	const [image, setImage] = useState(false);
+
+	const [productDetails, setProductDetails] = useState({
+		name: "",
+		image: "",
+		category: "men",
+		old_price: "",
+		new_price: "",
+	});
+
+	const changeHandler = (e) => {
+		setProductDetails({
+			...productDetails,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const imageHandler = (e) => {
+		setImage(e.target.files[0]);
+	};
+
+	const addProduct = async () => {
+		// console.log(productDetails);
+
+		let responseData;
+		let product = productDetails;
+
+		let formData = new FormData();
+		formData.append("product", image);
+
+		await fetch("http://localhost:4000/upload", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+			},
+			body: formData,
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				responseData = data;
+			});
+
+		if (responseData.success) {
+			product.image = responseData.image_url;
+			// console.log(product);
+
+			await fetch("http://localhost:4000/addproduct", {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(product),
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					data.success ? alert("Product added") : alert("Failed");
+				});
+		}
+	};
+
+	return (
+		<div className="add-product">
+			<div className="add-product-item-field">
+				<p>Product Title</p>
+				<input
+					value={productDetails.name}
+					onChange={changeHandler}
+					type="text"
+					name="name"
+					placeholder="Type here"
+				/>
+			</div>
+			<div className="add-product-price">
+				<div className="add-product-item-field">
+					<p>Price</p>
+					<input
+						value={productDetails.old_price}
+						onChange={changeHandler}
+						type="text"
+						name="old_price"
+						placeholder="Type here"
+					/>
+				</div>
+				<div className="add-product-item-field">
+					<p>Offer Price</p>
+					<input
+						value={productDetails.new_price}
+						onChange={changeHandler}
+						type="text"
+						name="new_price"
+						placeholder="Type here"
+					/>
+				</div>
+			</div>
+			<div className="add-product-item-field">
+				<p>Product Category</p>
+				<select
+					value={productDetails.category}
+					onChange={changeHandler}
+					name="category"
+					className="add-product-selector"
+				>
+					<option value="men">Men</option>
+					<option value="women">Women</option>
+					<option value="kids">Kids</option>
+				</select>
+			</div>
+			<div className="add-product-item-field">
+				<label htmlFor="file-input">
+					<img
+						src={image ? URL.createObjectURL(image) : uploadArea}
+						className="add-product-thumbnail-img"
+						alt=""
+					/>
+				</label>
+				<input type="file" name="image" id="file-input" onChange={imageHandler} hidden />
+			</div>
+			<button
+				onClick={() => {
+					addProduct();
+				}}
+				className="add-product-btn"
+			>
+				ADD
+			</button>
+		</div>
+	);
+};
+
+export default AddProduct;
---
+++ admin/src/components/ListProduct/ListProduct.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import "./ListProduct.css";
+import crossIcon from "../../assets/cross_icon.png";
+
+const ListProduct = () => {
+	const [allProducts, setAllProducts] = useState([]);
+
+	const fetchInfo = async () => {
+		try {
+			await fetch("http://localhost:4000/allproducts")
+				.then((res) => res.json())
+				.then((data) => {
+					setAllProducts(data);
+				});
+		} catch (error) {
+			console.log("Error fetching data:", error);
+		}
+	};
+
+	useEffect(() => {
+		fetchInfo();
+	}, []);
+
+	const removeProduct = async (id) => {
+		await fetch("http://localhost:4000/removeproduct", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ id: id }),
+		});
+    await fetchInfo();
+	};
+
+	return (
+		<div className="list-product">
+			<h1>All Products List</h1>
+			<div className="list-product-format-main">
+				<p className="ma">Products</p>
+				<p>Title</p>
+				<p className="ma">Old Price</p>
+				<p className="ma">New Price</p>
+				<p className="ma">Category</p>
+				<p className="ma">Remove</p>
+			</div>
+			<div className="list-product-all-product">
+				<hr />
+				{allProducts.map((product, i) => {
+					return (
+						<React.Fragment key={i}>
+							<div className="list-product-format-main list-product-format">
+								<img src={product.image} alt="" className="list-product-icon ma" />
+								<p>{product.name}</p>
+								<p className="ma">${product.old_price}</p>
+								<p className="ma">${product.new_price}</p>
+								<p className="ma">{product.category}</p>
+								<img src={crossIcon} alt="" className="list-product-remove ma" onClick={() => removeProduct(product.id)}/>
+							</div>
+							<hr />
+						</React.Fragment>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default ListProduct;
